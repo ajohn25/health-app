d1c50c256ac8fb0eea43af45f4276f9d90a8e6f2
thequantumking <ajohn4@pride.hofstra.edu>
2020-03-02 17:33:06 -0500

    Add Button Functions

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-let stephen_colbert = {
-    first_name: "Stephen",
-    last_name: "Hawking",
-    sayHello: function(other_name){
-        return "Hello " + other_name + " I'm " + this.first_name
-    }
+
+function clearInputs()
+{
+    document.getElementById("text_income_input").value = "Enter Your Income (in $)"
+    document.getElementById("statii").value = "Select Your Tax Status"
+    document.getElementById("provs").value = "Select Your Insurance Provider"
+    document.getElementById("plans").value = "Select Your Insurance Plan"
+    document.getElementById("text_fam_input").value = "How many family members do you have?"
+    
 }
 
-stephen_colbert.sayHello("Jim")
+function Advb(){
+    var inCheck = document.getElementById("depi");
+    if (inCheck.style.display === "inline-block") inCheck.style.display = "none";
+    else inCheck.style.display = "inline-block";
+}
