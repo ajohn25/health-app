0a9b5761003b0678c1425567fefb67c01685f71c
thequantumking <ajohn4@pride.hofstra.edu>
2020-03-03 00:18:54 -0500

    Full names for clarity

--- main.js
+++ main.js
@@ -9,8 +9,31 @@ function clearInputs()
     
 }
 
-function Advb(){
-    var inCheck = document.getElementById("depi");
+function hideSubmit()
+{
+    document.getElementById("text_income_input").style.display = "none";
+    document.getElementById("status_select").style.display = "none";
+    document.getElementById("providers_select").style.display = "none";
+    document.getElementById("plans_select").style.display = "none";
+    document.getElementById("text_fam_input").style.display = "none";
+    document.getElementById("placeholder").style.display = "none";
+    document.getElementById("clear").style.display = "none";
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("advanced").style.display = "none";
+    document.getElementById("results_headline").style.display = "inline-block";    
+    results_headline.style.display = "inline";
+}
+
+function toggleAdvanced() {
+    var inCheck = document.getElementById("advanced1_input");
     if (inCheck.style.display === "inline-block") inCheck.style.display = "none";
     else inCheck.style.display = "inline-block";
 }
+
+function SelectAll(id)
+{
+    document.getElementById(id).focus();
+    document.getElementById(id).select();
+}
+
+
