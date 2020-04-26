2d26da717a8d41e141df078b52f63ca831e7e8af
thequantumking <ajohn4@pride.hofstra.edu>
2020-04-26 15:39:17 -0400

    Error fixing

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 
 function clearInputs()
 {
-    document.getElementById("text_income_input").value = "Enter Your Income (in $)"
+    document.getElementById("income_text").value = "Enter Your Income (in $)"
     document.getElementById("statii").value = "Select Your Tax Status"
     document.getElementById("provs").value = "Select Your Insurance Provider"
     document.getElementById("plans").value = "Select Your Insurance Plan"
-    document.getElementById("text_fam_input").value = "How many family members do you have?"
+    document.getElementById("fam_text").value = "How many family members do you have?"
     
 }
 
 function hideSubmit()
 {
-    document.getElementById("text_income_input").style.display = "none";
+    document.getElementById("income_input").style.display = "none";
     document.getElementById("status_select").style.display = "none";
     document.getElementById("providers_select").style.display = "none";
     document.getElementById("plans_select").style.display = "none";
-    document.getElementById("text_fam_input").style.display = "none";
-    document.getElementById("placeholder").style.display = "none";
+    document.getElementById("fam_input").style.display = "none";
+    document.getElementById("cost_input").style.display = "none";
     document.getElementById("clear").style.display = "none";
     document.getElementById("submit").style.display = "none";
     document.getElementById("advanced_button").style.display = "none";
-    document.getElementById("advanced1").style.display = "none";
     document.getElementById("results_headline").style.display = "inline-block";    
     document.getElementById("restart_button").style.display = "inline-block";    
     document.getElementById("results_headline").style.display = "inline-block";    
@@ -33,16 +32,13 @@ function hideSubmit()
 
 function hideRestart()
 {
-    document.getElementById("text_income_input").style.display = "inline-block";
+    document.getElementById("income_input").style.display = "inline-block";
     document.getElementById("status_select").style.display = "inline-block";
-    document.getElementById("providers_select").style.display = "inline-block";
-    document.getElementById("plans_select").style.display = "inline-block";
-    document.getElementById("text_fam_input").style.display = "inline-block";
-    document.getElementById("placeholder").style.display = "inline-block";
+    document.getElementById("fam_input").style.display = "inline-block";
+    document.getElementById("cost_text").style.display = "inline-block";
     document.getElementById("clear").style.display = "inline-block";
     document.getElementById("submit").style.display = "inline-block";
     document.getElementById("advanced_button").style.display = "inline-block";
-    document.getElementById("advanced1").style.display = "inline-block";
     document.getElementById("results_headline").style.display = "none";    
     document.getElementById("restart_button").style.display =  "none"; 
     document.getElementById("results_headline").style.display = "none";    
@@ -54,9 +50,10 @@ function hideRestart()
 }
 
 function toggleAdvanced() {
-    var inCheck = document.getElementById("advanced1_input");
-    if (inCheck.style.display === "inline-block") inCheck.style.display = "none";
-    else inCheck.style.display = "inline-block";
+    var inCheck1 = document.getElementById("plan_input");
+    var inCheck2 = document.getElementById("insurance_input");
+    if (inCheck1.style.display == "inline-block") { inCheck1.style.display = "none"; inCheck2.style.display = "none"; }
+    else {inCheck1.style.display = "inline-block"; inCheck2.style.display = "inline-block";}
 }
 
 function SelectAll(id)
