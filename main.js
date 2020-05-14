b60fba6f3d352770e6c4e3e19c564fd6d34e6723
thequantumking <ajohn4@pride.hofstra.edu>
2020-05-14 11:51:59 -0400

    Cleanup Code

--- main.js
+++ main.js
@@ -1,59 +1,84 @@
-var savings = 0;
-function clearInputs()
-{
-    document.getElementById("income_text").value = "";
-    document.getElementById("status_select").value = "";
-    document.getElementById("fam_text").value = "";
-    document.getElementById("cost_text").value = "";
 
-    //document.getElementById("provs").value = "";
-    //document.getElementById("plans").value = "";    
+class Customer
+{
+    constructor(income, status, fam, cost)
+    {
+        this.tax = 0.04;
+        this.income = income;
+        this.status = status;
+        this.fam = fam;
+        this.cost = cost;
+        this.deduction = this.calcDeduction();
+   }
+
+    calcDeduction() 
+    {
+        if (this.status == 2 && this.fam > 3) return 29000;
+        else return 0;
+    }
+
+    calcNewCost()
+    {
+        var temp_cost = Math.round( (this.income - this.deduction) * this.tax );
+        if (temp_cost < 0) temp_cost = 0;
+        return temp_cost;
+    }
+
+    calcSavings() {return ( this.cost - this.calcNewCost() );}
 }
 
 function hideSubmit()
 {
+    //hiding
     document.getElementById("income_input").style.display = "none";
     document.getElementById("status_select").style.display = "none";
-    document.getElementById("providers_select").style.display = "none";
-    document.getElementById("plans_select").style.display = "none";
+    document.getElementById("insurance_input").style.display = "none";
+    document.getElementById("plan_input").style.display = "none";
     document.getElementById("fam_input").style.display = "none";
     document.getElementById("cost_input").style.display = "none";
     document.getElementById("clear").style.display = "none";
     document.getElementById("submit").style.display = "none";
     document.getElementById("advanced_button").style.display = "none";
+    //showing
     document.getElementById("results_headline").style.display = "inline-block";    
     document.getElementById("restart_button").style.display = "inline-block";    
     document.getElementById("results_headline").style.display = "inline-block";    
     document.getElementById("plan1_title").style.display = "inline-block";    
-   // document.getElementById("plan2_title").style.display = "inline-block";    
-    document.getElementById("plan3_title").style.display = "inline-block";    
-  //  document.getElementById("plan4_title").style.display = "inline-block";    
+    document.getElementById("plan2_title").style.display = "inline-block";  
+    document.getElementById("cost_title").style.display = "inline-block";  
+    document.getElementById("old_cost").style.display = "inline-block";  
+    document.getElementById("new_cost").style.display = "inline-block";  
     results_headline.style.display = "inline";
+    newPage();
+}
 
+function newPage() //Go to 2nd page
+{
     var income = document.getElementById("income_text").value;
     var status = document.getElementById("status_select").value;
     var fam = document.getElementById("fam_text").value;
     var cost = document.getElementById("cost_text").value;
     
-    var savings = calculate(income, status, fam, cost);
-    var verb_cost = "saves"
-    if (savings < 0) { verb_cost = "costs"; savings = -(savings);}
-    document.getElementById("savings_line").innerHTML = ("Medicare for All " + verb_cost + " you: $" + savings + " more");    
+    let User = new Customer(income, status, fam, cost); 
 
+    var plan_name = "Medicare for All"
+    var display_savings = User.calcSavings();
+
+    if (User.calcSavings() < 0) //Adjust text forp lan name if needed
+    { 
+        plan_name = "Your plan"; 
+        display_savings = -(display_savings);
+        document.getElementById("savings_wrap").style.color = "purple";
+    }
+    //Writing text to document
+    document.getElementById("savings_line").innerHTML = (plan_name + " saves you: $" + display_savings + " more");    
+    document.getElementById("old_cost_line").innerHTML = ("$" + User.cost );    
+    document.getElementById("new_cost_line").innerHTML = ("$" + User.calcNewCost() );
 }
 
-function calculate(income, status, fam, cost)
+function hideRestart() //Go back to 1st Page
 {
-    var reduce = 0;
-    if (status == 2 && fam > 3) reduce = 29000;
-    var new_cost = (income - reduce) * 0.04;
-    if(new_cost < 0) new_cost = 0;
-    var savings = Math.round( cost - new_cost);
-    return savings;
-}
-
-function hideRestart()
-{
+    //showing
     document.getElementById("income_input").style.display = "inline-block";
     document.getElementById("status_select").style.display = "inline-block";
     document.getElementById("fam_input").style.display = "inline-block";
@@ -61,13 +86,15 @@ function hideRestart()
     document.getElementById("clear").style.display = "inline-block";
     document.getElementById("submit").style.display = "inline-block";
     document.getElementById("advanced_button").style.display = "inline-block";
+    //hiding
     document.getElementById("results_headline").style.display = "none";    
     document.getElementById("restart_button").style.display =  "none"; 
     document.getElementById("results_headline").style.display = "none";    
     document.getElementById("plan1_title").style.display = "none";
-  //  document.getElementById("plan2_title").style.display = "none";
-    document.getElementById("plan3_title").style.display = "none";
- //   document.getElementById("plan4_title").style.display = "none";
+    document.getElementById("plan2_title").style.display = "none";
+    document.getElementById("cost_title").style.display = "none";
+    document.getElementById("old_cost").style.display = "none";
+    document.getElementById("new_cost").style.display = "none";
     results_headline.style.display = "none";
 }
 
@@ -79,6 +106,16 @@ function toggleAdvanced()
     else {inCheck1.style.display = "inline-block"; inCheck2.style.display = "inline-block";}
 }
 
+function clearInputs()
+{
+    document.getElementById("income_text").value = "";
+    document.getElementById("status_select").value = "";
+    document.getElementById("fam_text").value = "";
+    document.getElementById("cost_text").value = "";
+
+    //document.getElementById("provs").value = "";
+    //document.getElementById("plans").value = "";    
+}
 
 
 
